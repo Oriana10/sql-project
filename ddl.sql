fafc255731d3bee76707c216318423eeec9457d8
Oriana Pellegrini <oriana.pellegrini@wiener-lab.com>
2024-05-24 15:06:59 -0300

    Update tables

--- ddl.sql
+++ ddl.sql
@@ -3,31 +3,32 @@ CREATE DATABASE TUCONCIERTO;
 USE TUCONCIERTO;
 
 CREATE TABLE Cliente (
-    DNI VARCHAR(10) PRIMARY KEY,
+    DNI VARCHAR(8) NOT NULL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL
+    email VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE Estadio (
-    estadioID INT IDENTITY(1,1) PRIMARY KEY,
+    estadioID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     nombre VARCHAR(100)
 );
 
 CREATE TABLE Seccion (
-    seccionID INT IDENTITY(1,1) PRIMARY KEY,
+    seccionID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     capacidad_maxima INT NOT NULL,
     precio FLOAT NOT NULL,
-    ofrece_servicios BIT NOT NULL,
+    ofrece_servicios BIT NOT NULL DEFAULT 0,
     nombre VARCHAR(100) NOT NULL,
     estadioID INT NOT NULL,
     CONSTRAINT CK_Nombre check (nombre in ('Campo delantero','Campo trasero','Platea baja 1','Platea baja 2','Platea alta 1','Platea alta 2')),
+    CONSTRAINT CK_Precio check (precio >= 0)
     CONSTRAINT FK_Estadio FOREIGN KEY (estadioID) REFERENCES Estadio(estadioID)
 );
 
 
 CREATE TABLE Banda (
-    bandaID INT IDENTITY(1,1) PRIMARY KEY,
+    bandaID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL
 );
 
@@ -37,29 +38,29 @@ CREATE TABLE Asiento (
     fila VARCHAR(50) NOT NULL,
     columna VARCHAR(50) NOT NULL,
     esta_vendido BIT NOT NULL DEFAULT 0,
-    seccionID INT NULL,
-    FOREIGN KEY (FK_Seccion) REFERENCES Seccion(seccionID)
+    seccionID INT NOT NULL,
+    CONSTRAINT (FK_Seccion) FOREIGN KEY (seccionID) REFERENCES Seccion(seccionID)
 );
 
 CREATE TABLE Entrada (
-    compraId INT IDENTITY(1,1),
+    compraID INT NOT NULL,
     bandaID INT NOT NULL,
     asientoID INT NULL,
     seccionID INT NOT NULL,
     estadioID INT NOT NULL,
-    DNI VARCHAR(10) NOT NULL,
+    DNI_propietario VARCHAR(8) NOT NULL,
     precio FLOAT,
-	CONSTRAINT PK_Entrada  PRIMARY KEY (compraId, bandaID, estadioID),
-    CONSTRAINT fk_Cliente_Entrada FOREIGN KEY (DNI) REFERENCES Cliente(DNI),
+	CONSTRAINT PK_Entrada PRIMARY KEY (bandaID, estadioID, DNI_propietario),
+    CONSTRAINT fk_Compra_Entrada FOREIGN KEY (compraID) REFERENCES Compra(compraID)
     CONSTRAINT fk_Banda_Entrada FOREIGN KEY (bandaID) REFERENCES Banda(bandaID),
     CONSTRAINT fk_Asiento_Entrada FOREIGN KEY (asientoID) REFERENCES Asiento(asientoID),
     CONSTRAINT fk_Seccion_Entrada FOREIGN KEY (seccionID) REFERENCES Seccion(seccionID),
-    CONSTRAINT fk_Estadio_Entrada FOREIGN KEY (estadioID) REFERENCES Estadio(estadioID)
+    CONSTRAINT fk_Estadio_Entrada FOREIGN KEY (estadioID) REFERENCES Estadio(estadioID),
 );
 
 
 CREATE TABLE Compra (
-	campoID INT IDENTITY(1,1) PRIMARY KEY,
+	compraID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	fechaCompra DATE NOT NULL,
     FOREIGN KEY (FK_Cliente) REFERENCES Cliente(DNI),
 )
